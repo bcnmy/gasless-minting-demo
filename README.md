a0839ee31084c6840a55c9016809cbd08a38c324
AbhinavXT <abhinavpathaka17@gmail.com>
2022-04-20 17:02:17 +0530

    custom EIP-712 approach

--- README.md
+++ README.md
@@ -1,1 +1,65 @@
-# gasless-minting-demo
+# Gasless-NFT
+
+## Table of Contents
+- [Deployed Website url](#deployed-website-url)
+- [Deployed Contract Address](#deployed-contract-address)
+- [Clone, Install and Build steps](#clone-install-and-build-steps)
+  - [Prerequisites](#prerequisites)
+  - [Cloning and installing dependencies](#cloning-and-installing-dependencies)
+  - [Running the frontend](#running-the-frontend)
+  - [Environment variables](#environment-variables)
+
+## Deployed Website url
+
+https://gasless-nft.vercel.app/
+
+## Deployed Contract Address
+
+`0x372d3e535fb9FCABF44df1fEBeb7d8749e189512`
+
+## Clone, Install and Build steps
+
+### Prerequisites
+
+1. [Git](https://git-scm.com/)
+2. [Node JS](https://nodejs.org/en/) (everything was installed and tested under v16.13.0)
+3. A Browser with the [MetaMask extension](https://metamask.io/) installed.
+
+<br>
+
+### Cloning and installing dependencies
+
+1. Clone the project repository on your local machine
+
+```
+ git clone https://github.com/AbhinavXT/Gasless-NFT.git
+ cd Gasless-NFT
+```
+
+2. Installing dependencies
+
+-   For contracts -
+    ```
+    npm install
+    ```
+-   For client -
+    ```
+    cd client
+    npm install
+    ```
+
+### Running the frontend
+
+For running frontend locally run command:
+
+```
+cd client
+npm run dev
+```
+
+### Environment variables
+
+```
+ALCHEMY_KOVAN_URL = ''
+ACCOUNT_KEY = ''
+```
---
+++ test/test.js
@@ -0,0 +1,125 @@
+const { assert } = require("chai")
+const sigUtil = require("eth-sig-util")
+
+require("dotenv").config()
+
+const EIP712MetaTransaction = require("../artifacts/contracts/EIP712MetaTransaction.sol/EIP712MetaTransaction.json")
+const NFT = require("../artifacts/contracts/EternalNFT.sol/EternalNFT.json")
+
+let wallet = new ethers.Wallet.createRandom()
+let publicKey = wallet.address
+let privateKey = wallet._signingKey().privateKey
+
+const domainType = [
+	{ name: "name", type: "string" },
+	{ name: "version", type: "string" },
+	{ name: "verifyingContract", type: "address" },
+	{ name: "salt", type: "bytes32" },
+]
+
+const metaTransactionType = [
+	{ name: "nonce", type: "uint256" },
+	{ name: "from", type: "address" },
+	{ name: "functionSignature", type: "bytes" },
+]
+
+let domainData
+
+const getTXData = async (nonce, functionSignature) => {
+	let message = {
+		nonce: parseInt(nonce),
+		from: publicKey,
+		functionSignature: functionSignature,
+	}
+
+	let dataToSign = {
+		types: {
+			EIP712Domain: domainType,
+			MetaTransaction: metaTransactionType,
+		},
+		domain: domainData,
+		primaryType: "MetaTransaction",
+		message: message,
+	}
+
+	let signature = sigUtil.signTypedData(
+		new Buffer.from(privateKey.substring(2, 66), "hex"),
+		{ data: dataToSign },
+		"V3"
+	)
+
+	let r = signature.slice(0, 66)
+	let s = "0x".concat(signature.slice(66, 130))
+	let v = "0x".concat(signature.slice(130, 132))
+	v = ethers.BigNumber.from(v).toNumber()
+	if (![27, 28].includes(v)) v += 27
+
+	return {
+		r,
+		s,
+		v,
+	}
+}
+
+describe("NFT Contract", async () => {
+	let nftContractFactory,
+		nftContract,
+		metaTransactionContractFactory,
+		metaTransactionContract,
+		nftContractInterface
+
+	before("before test", async () => {
+		nftContractFactory = await ethers.getContractFactory("EternalNFT")
+		nftContract = await nftContractFactory.deploy()
+		await nftContract.deployed()
+
+		nftContractInterface = new ethers.utils.Interface(NFT.abi)
+
+		metaTransactionContractFactory = await ethers.getContractFactory(
+			"EIP712MetaTransaction"
+		)
+		metaTransactionContract = await metaTransactionContractFactory.deploy(
+			"EternalNFT",
+			"1"
+		)
+		await metaTransactionContract.deployed()
+
+		const [owner, _] = await ethers.getSigners()
+
+		domainData = {
+			name: "EternalNFT",
+			version: "1",
+			verifyingContract: nftContract.address,
+			salt: ethers.utils.hexZeroPad(
+				ethers.BigNumber.from(1337).toHexString(),
+				32
+			),
+		}
+	})
+
+	it("Should be able to send transaction successfully", async () => {
+		let nonce = await nftContract.getNonce(publicKey)
+
+		let functionSignature =
+			nftContractInterface.encodeFunctionData("createEternalNFT")
+
+		const { r, s, v } = await getTXData(nonce, functionSignature)
+
+		let tx = await nftContract.executeMetaTransaction(
+			publicKey,
+			functionSignature,
+			r,
+			s,
+			v
+		)
+
+		const txData = await tx.wait(1)
+		const tokenId = txData.events[0].args.tokenId.toString()
+
+		console.log("Transaction hash : ", tx.hash)
+
+		let newNonce = await nftContract.getNonce(publicKey)
+
+		assert.isTrue(newNonce.toNumber() == nonce + 1, "Nonce not incremented")
+	})
+})
